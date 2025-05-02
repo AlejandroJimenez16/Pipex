b1181928f19669a53ea4ca7cc6025601e29bdac5
AlejandroJimenez16 <alejanjiga@gmail.com>
2025-05-02 20:17:56 +0200

    Añadida funcion free_arr, procesos creados y gestion errores

--- src/pipex.c
+++ src/pipex.c
@@ -6,11 +6,26 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 16:53:38 by alejandj          #+#    #+#             */
-/*   Updated: 2025/04/29 14:57:44 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/05/02 20:16:36 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../includes/pipex.h"
+
+void	free_arr(char **arr)
+{
+	int	i;
+
+	if (!arr)
+		return ;
+	i = 0;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
 
 int	main(int argc, char *argv[], char *env[])
 {
@@ -24,6 +39,7 @@ int	main(int argc, char *argv[], char *env[])
 	int		pipefd[2];
 
 	pid_t	process1;
+	pid_t	process2;
 	
 	if (argc == 5)
 	{
@@ -31,9 +47,15 @@ int	main(int argc, char *argv[], char *env[])
 		fd_in = open(argv[1], O_RDONLY);
 		fd_out = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 		if (fd_in < 0)
+		{
 			perror("Error opening infile");
+			exit(EXIT_FAILURE);
+		}	
 		if (fd_out < 0)
+		{
 			perror("Error opening outfile");
+			exit(EXIT_FAILURE);
+		}
 		
 		// Parsear los comandos
 		cmd1 = ft_split(argv[2], ' ');
@@ -43,13 +65,37 @@ int	main(int argc, char *argv[], char *env[])
 		pipe_status = pipe(pipefd);
 		if (pipe_status == -1)
 		{
-			perror("\033[1;31mError creating pipe\033[0m\n");
+			perror("Error creating pipe");
 			exit(EXIT_FAILURE);
 		}
 
 		// Crear procesos
 		process1 = fork();
-		
+		if (process1 == -1)
+		{
+			perror("Error creating process1");
+			exit(EXIT_FAILURE);
+		}
+		else if (process1 == 0)
+		{
+			ft_printf("Soy un proceso hijo %d\n", process1);
+			exit(0);
+		}
+		process2 = fork();
+		if (process2 == -1)
+		{
+			perror("Error creating process2");
+			exit(EXIT_FAILURE);
+		}
+		else if (process2 == 0)
+		{
+			ft_printf("Soy un proceso hijo %d\n", process2);
+			exit(0);
+		}
+
+		// Liberar
+		free_arr(cmd1);
+		free_arr(cmd2);
 	}
 	else
 	{

a3de8f10f3f7807ee53b56126f6af9bc7a4353ce
AlejandroJimenez16 <alejanjiga@gmail.com>
2025-05-04 14:40:54 +0200

    Estructura y funciones

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 12:32:34 by alejandj          #+#    #+#             */
-/*   Updated: 2025/04/28 16:01:22 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/05/04 14:22:19 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,22 @@
 # include <stdio.h>
 # include <string.h>
 # include <sys/wait.h>
+
+typedef struct s_pipex
+{
+    int		fd_in;
+    int		fd_out;
+    int		pipefd[2];
+	char	**cmd1;
+	char	**cmd2;
+	char	**env;
+	pid_t	process1;
+	pid_t	process2;
+} t_pipex;
+
+
+void	free_arr(char **arr);
+void	print_errors(char *error);
+char	**get_path_cmd(char *env[]);
 
 #endif

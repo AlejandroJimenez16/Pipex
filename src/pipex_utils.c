72caaf103f39b01d4912b883ca3655d8e15db8dc
Alejandro Jimenez Garcia <alejandj@c2r14s1.42madrid.com>
2025-05-05 17:09:09 +0200

    Mas funciones

--- src/pipex_utils.c
+++ src/pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 12:52:47 by alejandj          #+#    #+#             */
-/*   Updated: 2025/05/04 14:04:21 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/05/05 16:55:09 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,28 +27,19 @@ void	free_arr(char **arr)
 	free(arr);
 }
 
-void	print_errors(char *error)
+void	print_errors(char *error, char *outfile)
 {
 	ft_putstr_fd("\033[1;31m", 2);
 	ft_putstr_fd(error, 2);
 	ft_putstr_fd("\033[0m\n", 2);
+	unlink(outfile);
 	exit(EXIT_FAILURE);
 }
 
-char	**get_path_cmd(char *env[])
+void	fail_args(void)
 {
-	int		i;
-	char	**arr_path;
-
-	i = 0;
-	while (env[i] != NULL)
-	{
-		if (ft_strncmp(env[i], "PATH=", 5) == 0)
-		{
-			arr_path = ft_split(env[i] + 5, ':');
-			break ;
-		}
-		i++;
-	}
-	return (arr_path);
+	ft_putstr_fd("\033[1;31mERROR ARGUMENTS\033[0m\n", 2);
+	ft_putstr_fd("\033[1;33m", 2);
+	ft_putstr_fd("ARGS FORMAT: ./pipex infile 'cmd1' 'cmd2' outfile", 2);
+	ft_putstr_fd("\033[0m\n", 2);
 }

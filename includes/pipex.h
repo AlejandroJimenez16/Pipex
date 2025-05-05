be20bc8e4363ff17fe2d79d7388eb2a7de9357aa
Alejandro Jimenez Garcia <alejandj@c2r14s1.42madrid.com>
2025-05-05 17:07:46 +0200

    Normi ok y mas funciones

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 12:32:34 by alejandj          #+#    #+#             */
-/*   Updated: 2025/05/04 14:22:19 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/05/05 15:45:57 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,29 @@
 
 # include "../libft/libft.h"
 # include <fcntl.h>
-# include <unistd.h>
-# include <stdlib.h>
 # include <stdio.h>
+# include <stdlib.h>
 # include <string.h>
 # include <sys/wait.h>
+# include <unistd.h>
 
 typedef struct s_pipex
 {
-    int		fd_in;
-    int		fd_out;
-    int		pipefd[2];
+	int		fd_in;
+	int		fd_out;
+	int		pipefd[2];
 	char	**cmd1;
 	char	**cmd2;
+	char	*outfile;
 	char	**env;
 	pid_t	process1;
 	pid_t	process2;
-} t_pipex;
+}			t_pipex;
 
-
-void	free_arr(char **arr);
-void	print_errors(char *error);
-char	**get_path_cmd(char *env[]);
+void		free_arr(char **arr);
+void		print_errors(char *error, char *outfile);
+void		fail_args(void);
+char		**get_path_cmd(char *env[]);
+void		execute_commands(char *env[], char **cmd, char *outfile);
 
 #endif

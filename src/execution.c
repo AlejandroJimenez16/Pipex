efe89b237d762ad7fa0eaeacb85998a27447d6e8
Alejandro Jimenez Garcia <alejandj@c2r14s1.42madrid.com>
2025-05-05 17:08:41 +0200

    Ejecucion comandos

---
+++ src/execution.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 13:37:27 by alejandj          #+#    #+#             */
+/*   Updated: 2025/05/05 16:30:52 by alejandj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+char	**get_path_cmd(char *env[])
+{
+	int		i;
+	char	**arr_path;
+
+	i = 0;
+	arr_path = NULL;
+	while (env[i] != NULL)
+	{
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+		{
+			arr_path = ft_split(env[i] + 5, ':');
+			break ;
+		}
+		i++;
+	}
+	return (arr_path);
+}
+
+void	execute_commands(char *env[], char **cmd, char *outfile)
+{
+	char	**arr_path;
+	int		arr_path_id;
+	char	*path;
+	char	*temp;
+
+	path = ft_strdup("");
+	arr_path = get_path_cmd(env);
+	arr_path_id = 0;
+	while (arr_path[arr_path_id] != NULL)
+	{
+		temp = ft_strjoin(arr_path[arr_path_id], "/");
+		path = ft_strjoin(temp, cmd[0]);
+		if (access(path, X_OK) == 0)
+		{
+			if (execve(path, cmd, env) == -1)
+				print_errors("ERROR: execve process1", outfile);
+			break ;
+		}
+		else
+			arr_path_id++;
+	}
+	print_errors("ERROR: Incorrect command", outfile);
+}
